5e1b658f3104b5d1c03f61861d145c2f14539f8c
Luis Eduardo Falcao <eduardo.cfalcao@gmail.com>
2021-12-20 15:22:28 -0300

    Makes the CaptureOnlyLogger implements the Logger interface.

--- pkg/logger/capturing_logger.go
+++ pkg/logger/capturing_logger.go
@@ -2,7 +2,10 @@ package logger
 
 // Copyright (C) 2021 by RStudio, PBC.
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 // CapturingLogger is a direct logger that remembers _all_ its logged
 // messages. Useful for tests.
@@ -42,4 +45,48 @@ func (logger *CaptureOnlyLogger) Output(msg string) {
 
 func (logger *CaptureOnlyLogger) Reset() {
 	logger.Messages = nil
+}
+
+func (logger *CaptureOnlyLogger) Debugf(msg string, args ...interface{}) {
+	logger.Messages = append(logger.Messages, msg)
+}
+
+func (logger *CaptureOnlyLogger) Infof(msg string, args ...interface{}) {
+	logger.Messages = append(logger.Messages, msg)
+}
+
+func (logger *CaptureOnlyLogger) Warnf(msg string, args ...interface{}) {
+	logger.Messages = append(logger.Messages, msg)
+}
+
+func (logger *CaptureOnlyLogger) Errorf(msg string, args ...interface{}) {
+	logger.Messages = append(logger.Messages, msg)
+}
+
+func (logger *CaptureOnlyLogger) Fatalf(msg string, args ...interface{}) {
+	logger.Messages = append(logger.Messages, msg)
+}
+
+func (logger *CaptureOnlyLogger) Fatal(args ...interface{}) {
+}
+
+func (logger *CaptureOnlyLogger) WithField(key string, value interface{}) Logger {
+	return logger
+}
+
+func (logger *CaptureOnlyLogger) WithFields(fields Fields) Logger {
+	return logger
+}
+
+func (logger *CaptureOnlyLogger) SetLevel(_ LogLevel) {
+}
+
+func (logger *CaptureOnlyLogger) SetReportCaller(bool) {
+}
+
+func (logger *CaptureOnlyLogger) Copy() Logger {
+	return logger
+}
+
+func (logger *CaptureOnlyLogger) SetOutput(writers ...io.Writer) {
 }
