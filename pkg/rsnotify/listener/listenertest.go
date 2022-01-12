4cf2da70d4cbc461b6bd0480e347ae78a718cd89
Jonathan Yoder <jonyoder@gmail.com>
2022-01-12 10:26:07 -0500

    Update Copyright Notices to new format

--- pkg/rsnotify/listener/listenertest.go
+++ pkg/rsnotify/listener/listenertest.go
@@ -1,18 +1,6 @@
 package listener
 
-/* listenertest.go
- *
- * Copyright (C) 2021 by RStudio, PBC
- * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains the property of
- * RStudio, PBC and its suppliers, if any. The intellectual and technical
- * concepts contained herein are proprietary to RStudio, PBC and its suppliers
- * and may be covered by U.S. and Foreign Patents, patents in process, and
- * are protected by trade secret or copyright law. Dissemination of this
- * information or reproduction of this material is strictly forbidden unless
- * prior written permission is obtained.
- */
+// Copyright (C) 2022 by RStudio, PBC.
 
 import "log"
 
