90b37e89000aa49dbbee253bbd58a111d840dcfd
Zack Verham <zack.verham@rstudio.com>
2022-02-01 17:50:23 -0500

    fixing some issues with the fatal call in LoggerMock, and added some tests to validate

--- pkg/rslog/rslogtest/mocks.go
+++ pkg/rslog/rslogtest/mocks.go
@@ -40,7 +40,12 @@ type LoggerMock struct {
 // allowing to be called with any message and arguments
 func (m *LoggerMock) AllowAny(methods ...string) {
 	for _, method := range methods {
-		m.On(method, mock.AnythingOfType("string"), mock.Anything)
+		//todo: I wonder if these should be types rather than string inputs
+		if method == "Fatal" {
+			m.On(method, mock.Anything)
+		} else {
+			m.On(method, mock.AnythingOfType("string"), mock.Anything)
+		}
 	}
 }
 
@@ -117,6 +122,7 @@ func (m *LoggerMock) Errorf(msg string, args ...interface{}) {
 }
 
 func (m *LoggerMock) Fatal(args ...interface{}) {
+	m.stringCalls = append(m.stringCalls, fmt.Sprint(args...))
 	m.Called(args)
 }
 
