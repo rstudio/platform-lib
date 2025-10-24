05cd659b08b2e06e8cfe68022fcd37f9ae35124f
Clay Raynor <clay.raynor@posit.co>
2025-10-24 11:40:42 -0400

    Update FakeAgent methods to use context

--- pkg/rsqueue/agent/agenttest.go
+++ pkg/rsqueue/agent/agenttest.go
@@ -3,6 +3,7 @@ package agent
 // Copyright (C) 2022 by RStudio, PBC
 
 import (
+	"context"
 	"time"
 
 	agenttypes "github.com/rstudio/platform-lib/v2/pkg/rsqueue/agent/types"
@@ -13,13 +14,13 @@ type FakeAgent struct {
 	WaitResult uint64
 }
 
-func (a *FakeAgent) Run(notify agenttypes.Notify) {
+func (a *FakeAgent) Run(ctx context.Context, notify agenttypes.Notify) {
 }
 
 func (a *FakeAgent) Stop(timeout time.Duration) error {
 	return a.StopErr
 }
 
-func (a *FakeAgent) Wait(runningJobs int64, jobDone chan int64) uint64 {
+func (a *FakeAgent) Wait(ctx context.Context, runningJobs int64, jobDone chan int64) uint64 {
 	return a.WaitResult
 }
